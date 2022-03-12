caae25705ff1dec6da7c15f4a6ea67f68b2d046c
Kristina Chausheva <k.chausheva@gmail.com>
2022-03-12 03:03:48 +0200

    adding some dummy data to the List component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -7,7 +7,10 @@ import {
   MenuItem,
   FormControl,
   Select,
+  Card,
 } from "@material-ui/core"
+
+import PlaceDetails from "../PlaceDetails/PlaceDetails"
 
 import useStyles from "./styles"
 
@@ -15,6 +18,12 @@ export default function List() {
   const classes = useStyles()
   const [type, setType] = useState("restaurants")
   const [rating, setRating] = useState("")
+
+  const places = [
+    { name: "Cool Place" },
+    { name: "Best Beer" },
+    { name: "Best Steak" },
+  ]
 
   return (
     <div className={classes.container}>
@@ -38,6 +47,13 @@ export default function List() {
           <MenuItem value={4.5}>Above 4.5</MenuItem>
         </Select>
       </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+            <PlaceDetails place={place} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
