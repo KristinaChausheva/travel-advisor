0fddee696e27edd047b537e0e900bc4d778d13d1
Kristina Chausheva <k.chausheva@gmail.com>
2022-03-12 02:56:29 +0200

    implementing List component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,19 +1,20 @@
 import React from "react"
 import GoogleMapReact from "google-map-react"
 import { Paper, Typography, useMediaQuery } from "@material-ui/core"
-import LocationOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined"
 import Rating from "@material-ui/lab"
 
 import useStyles from "./styles"
 
 export default function Map() {
+  const GOOGLE_API = ""
   const classes = useStyles()
   const isMobile = useMediaQuery("(min-width:600px)") //isMobile will be false if the screen is larger than the given px
   const dummyCoords = { lat: 0, lng: 0 }
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
+        bootstrapURLKeys={{ key: GOOGLE_API }}
         defaultCenter={dummyCoords}
         center={dummyCoords}
         defaultZoom={14}
